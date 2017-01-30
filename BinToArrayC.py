d4877c2d959f6e9c72fac5f97827efdf3dc467ae
Damian Pala <mail@damianpala.com>
2017-01-30 19:48:57 +0100

    Corrected file open mode and added leading zero in array items.

--- BinToArrayC.py
+++ BinToArrayC.py
@@ -10,7 +10,7 @@ import sys
 if len(sys.argv) > 1:
     input_file_name = sys.argv[1]
 else:
-    input_file_name = "binfile.bin"
+    input_file_name = "Nucleo_blink_led_NUCLEO_L476RG.bin"
 scritDirectory = os.path.dirname(sys.argv[0])
 
 
@@ -18,32 +18,34 @@ output_file_name = "BinArray.c"
 
 bin_list = []
 
-with open(input_file_name, 'r') as f:
-    byte = f.read(1)
-    while byte != "":
-        bin_list.append(byte)
-        byte = f.read(1)
-    
+with open(input_file_name, 'rb') as f:
+    while True:
+        b = f.read(1)
+        if not b:
+            # eof
+            break
+        bin_list.append(b)
+
 
 output_content = ""
 output_content += "#include \"stdint.h\"\n\n"
 output_content += "static uint32_t BinArraySize = " + str(len(bin_list)) + "\n"
 output_content += "static uint8_t BinArray[] =\n{ \n"
 output_content += "  "
-
+ 
 item_iterator = 0
 for item in bin_list:
-    byte_string = hex(ord(item))
+    byte_string = '0x' + hex(ord(item))[2:].zfill(2)
     output_content += byte_string + ", "
     item_iterator += 1
     if item_iterator == 20:
         output_content += "\n  "
         item_iterator = 0
-    
+     
 output_content = output_content[:-2]
 output_content += "\n" 
 output_content += "};"
-
+ 
 print output_content
-
+ 
 open(scritDirectory + '\\' + output_file_name, 'wb').write(output_content)
